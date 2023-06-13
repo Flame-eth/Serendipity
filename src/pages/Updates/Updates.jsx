4e1e179ebc3fe796df6e23f9c24dba679fc203ee
Flame-eth <theflamepen@gmail.com>
2023-06-13 19:18:43 +0100

    updates page

--- src/pages/Updates/Updates.jsx
+++ src/pages/Updates/Updates.jsx
@@ -3,7 +3,7 @@ import "./Updates.scss";
 import Navbar from "../../components/Navbar/Navbar";
 import { navLinks } from "../../constants";
 import { Link } from "react-router-dom";
-import { Logo } from "../../assets";
+import { Logo, linkedDark } from "../../assets";
 
 const Updates = () => {
   return (
@@ -27,7 +27,46 @@ const Updates = () => {
             </div>
           </div>
         </div>
-        <div className="sidebar"></div>
+        <div className="sidebar">
+          <div className="titleSec">
+            <h1>Updates</h1>
+            <p>
+              Stay up-to-date with the latest features released on Serendipity.
+              Our most recent updates are now live, offering you a wide range of
+              new features and enhancements that will enhance your experience.
+              From sleek design changes to powerful new tools, we are
+              continuously working to make our platform even better for you.
+              Discover the latest updates today and take your experience to the
+              next level!
+            </p>
+          </div>
+          <div className="cardSec">
+            <div className="icon">
+              <img src={linkedDark} alt="" />
+            </div>
+            <div className="text">
+              <div className="mainText">
+                <div className="title">Course</div>
+                <div className="subTitle">
+                  <p>OPTIMUS WEB</p>
+                  <span>FIXED - APRIL 21, 2023</span>
+                </div>
+              </div>
+              <div className="subText">
+                <li>
+                  <ol>
+                    Variations on image prompts without text prompts now work
+                    again.
+                  </ol>
+                  <ol>
+                    Variations on image prompts without text prompts now work
+                    again.
+                  </ol>
+                </li>
+              </div>
+            </div>
+          </div>
+        </div>
 
         <div className="bottomMenu"></div>
       </div>
